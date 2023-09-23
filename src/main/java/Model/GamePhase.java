7584fecdb2bc55065ba4b160b80281954878059b
RancyKaur <rancy.chadha@gmail.com>
2023-09-23 19:57:01 -0400

    Created basic layout to work on maps

---
+++ src/main/java/Model/GamePhase.java
@@ -0,0 +1,47 @@
+package Model;
+
+/**
+ * GamePhases is an enum of the several phases that the game moves through
+ * As per the Warzone project description we have defined the phases
+ */
+public enum GamePhase {
+    /**
+     * BEGIN phase is the first phase when the sample maps are loaded and displayed to the player
+     * User can decide to move to next phase of either EDITMAP or STARTPLAY
+     */
+    BEGINGAME,
+
+    /**
+     * EDITMAP phase indicates that player can edit existing map or create new map
+     * This phase starts when player enters the command 'editmap'
+     * The phase ends when player enters 'loadmap' or 'stop'
+     */
+    EDITMAP,
+
+    /**
+     * This phase starts when player enters the command 'loadmap'
+     * In this phase players are assigned to the game using 'gameplayer' command
+     * The map is selected with loadmap command
+     * 'assigncountries' command leads to end of this phase and start next phase
+     */
+    STARTPLAY,
+
+    /**
+     * This phase starts when player executes 'deploy' command
+     * That would add the orders to the order list until all reinforcement armies are deployed
+     * It ends after all players have played their turn in round-robin
+     */
+    ISSUEORDER,
+
+    EXECUTEORDER,
+
+    /**
+     * TAKETURN is a signal for end of current player's turn and start of next player's turn.
+     */
+    TAKETURN,
+
+    /**
+     * This is signal for ending the game, when the player enter the command 'stopgame'
+     */
+    ENDGAME
+}
--- src/main/java/View/GamePlay.java
+++ src/main/java/View/GamePlay.java
@@ -1,8 +1,51 @@
 package View;
 
+import Controller.GameEngine;
+import Model.GamePhase;
+import java.util.Scanner;
+
+
+/**
+ * GamePlay is the entry point of the program and is the view that is presented to the players
+ * It calls the controller GameEngine to issue the commands passed by the player
+ */
 public class GamePlay {
-    public static void main(String[] args)
+
+    Scanner d_inp = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("****************************************");
+        System.out.println("Welcome to the WARZONE game");
+        System.out.println("****************************************");
+        System.out.println();
+        GamePlay l_game = new GamePlay();
+
+        System.out.println("You can start the game by EITHER: Selecting an existing map as given below or Creating new map");
+        System.out.println("At any point if you type 'stopgame' the game would be stopped");
+        l_game.displaySavedMaps();
+        System.out.println("Type 'editmap <name of map> without extension, if the map name is not part of above list, a new map will be created");
+        l_game.enterGamePhases();
+    }
+
+    /**
+     * As the game moves in phases, this method is called as soon as the player enters their first command
+     * This would in-turn call GameEngine controller that would execute commands
+     */
+    private void enterGamePhases()
     {
-        System.out.println("Welcome to the game");
+        GameEngine l_cmd = new GameEngine();
+        String l_command = d_inp.nextLine();
+        GamePhase l_phase = l_cmd.parseCommand(l_command);
+
+        while( l_phase!=GamePhase.ENDGAME || l_phase!=GamePhase.ISSUEORDER)
+        {
+            l_command = d_inp.nextLine();
+            l_phase = l_cmd.parseCommand(l_command);
+            System.out.println("Waiting for next command..");
+        }
+    }
+
+    private void displaySavedMaps()
+    {
+
     }
 }

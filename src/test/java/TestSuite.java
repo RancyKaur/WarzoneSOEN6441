b59ce24848b29d6fa5f1969790bcf25a63ae90bd
Alekhya K <jahnavialekhya2000@gmail.com>
2023-09-30 05:41:59 +0530

    Test cases for Country and Continent

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,21 @@
+import model.*;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+//        TestContinent.class,
+		TestCountry.class
+})
+
+	/**
+	 * It runs a collection of test cases.
+	 *
+	 */
+	public class TestSuite {
+    //Driver for All Test Cases
+	}
+
+

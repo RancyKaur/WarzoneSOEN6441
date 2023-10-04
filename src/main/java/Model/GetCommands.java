2e6a16d269685cdc4dbfc5e0953c43dfbe71d822
ronakjpatel <40088060+ronakjpatel@users.noreply.github.com>
2023-10-04 19:42:45 -0400

    Did some necessary changes like code logic  and added more informative user guide print messsages

--- src/main/java/Model/GetCommands.java
+++ src/main/java/Model/GetCommands.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class GetCommands {
     private static String[] d_validBeginPhaseCmds = new String[]{"editmap", "stopgame", "loadmap"};
     private static String[] d_validEditMapPhaseCmds = new String[]{"editcontinent", "editcountry", "editneighbor", "validatemap", "savemap", "showmap", "loadmap", "stopgame"};
-    private static String[] d_validateStartPlayCmds = new String[]{"gameplayer", "assigncountries"};
+    private static String[] d_validateStartPlayCmds = new String[]{"gameplayer", "assigncountries","showmap"};
     private static String[] d_validIssueOrderCmds = new String[]{"deploy"};
     private static Scanner d_scan;
 

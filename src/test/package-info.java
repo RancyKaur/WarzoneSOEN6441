4d90d2ffc926d35860686aa06a2de962d597f1b8
Sujith <94170693+Luna-martinez@users.noreply.github.com>
2023-10-04 23:36:38 -0400

    src/test

---
+++ src/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This class represents a test suite that runs a collection of test cases for the Risk Game application.
+ * It is used to organize and execute various tests related to different aspects of the game.
+ */
+

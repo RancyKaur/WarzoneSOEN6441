75d5d71344787c1e2826e6c902da1236cfa820a4
Chhabra-Jatin <jatin.chhabra772@gmail.com>
2023-10-26 08:09:09 -0400

    added Card.java for handling implementation related to cards

--- src/main/java/Model/Card.java
+++ src/main/java/Model/Card.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.io.Serializable;
 import java.util.Random;
+
 /**
  * Class handles implementation related to cards.
  */
@@ -10,56 +11,61 @@ public class Card implements Serializable {
     /**
      * Represents type of Card from Bomb, Airlift, Blockade and Diplomacy
      */
-    String d_CardType;
+    private String d_CardType;
 
     /**
      * This list contains the cards issued to players
      */
-    String[] d_CardsList = {"Bomb","Airlift","Blockade","Diplomacy"};
-
-    /**
-     *The method gives type of a card.
-     * @return string card type
-     */
-    String getCardType()
-    {
-        return d_CardType;
-    }
+    private static final String[] d_CardsList = { "Bomb", "Airlift", "Blockade", "Diplomacy" };
 
     /**
      * Default constructor of Card to access the methods of this class.
      */
-    public Card(){
+    public Card() {
     }
 
     /**
      * This constructor will assign type of cards
+     * 
      * @param p_cardType Card Type that is assigned
      */
-    public Card(String p_cardType){
-        this.d_CardType=p_cardType;
+    public Card(String p_cardType) {
+        this.d_CardType = p_cardType;
     }
+
+    /**
+     * The method gives type of card.
+     * 
+     * @return string card type
+     */
+    String getCardType() {
+        return d_CardType;
+    }
+
     /**
      * Stores the random card picked in the CardType String
      */
-    public void createCard()
-    {
+    public void createCard() {
         d_CardType = randomCard();
     }
 
     /**
      * Stores the temp card picked in the CardType String
+     * 
      * @param temp specific card
      */
-    public void createCard(String temp) { d_CardType = temp; }
+    public void createCard(String temp) {
+        d_CardType = temp;
+    }
+
     /**
      * Picks a random card from the Cards List using random generator
+     * 
      * @return The index of the Cards List
      */
-    public String randomCard()
-    {
-        Random randomGenerator = new Random();
-        int index = randomGenerator.nextInt(d_CardsList.length);
+    public String randomCard() {
+        Random randomCardGenerator = new Random();
+        int index = randomCardGenerator.nextInt(d_CardsList.length);
         return d_CardsList[index];
     }
 

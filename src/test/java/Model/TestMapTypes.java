eb0d6761a23d290bbd659d2da5361d7b33b4831f
Chhabra-Jatin <jatin.chhabra772@gmail.com>
2023-11-23 12:34:54 -0500

    TestMapTypes test file added

---
+++ src/test/java/Model/TestMapTypes.java
@@ -0,0 +1,26 @@
+package Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test case to confirm type of map
+ *
+ */
+public class TestMapTypes {
+
+    LoadGraph d_LoadMap;
+    String d_MapName, d_MapType;
+
+    /**
+     * set up context
+     */
+    @Before
+    public void before(){
+        d_LoadMap= new LoadGraph();
+    }
+
+    
+}
